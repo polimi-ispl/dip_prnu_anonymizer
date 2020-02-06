2f61fbe1bff156ef5409a1f0891ed5c1b8674486
Sara Mandelli <smandell91@gmail.com>
2020-02-06 17:36:15 +0100

    Add results_script

---
+++ results_script/save_results.py
@@ -0,0 +1,76 @@
+# view output results from DEEP PRNU ANONYMIZER:
+
+import os
+import matplotlib.pyplot as plt
+from glob import glob
+import numpy as np
+from collections import namedtuple
+History = namedtuple("History", ['loss', 'psnr', 'ssim', 'ncc_w', 'ncc_d'])
+from scipy.io import loadmat
+
+# devices:
+devices = ['Nikon_D200_0', 'Nikon_D200_1', 'Nikon_D70_0', 'Nikon_D70_1', 'Nikon_D70s_0', 'Nikon_D70s_1']
+
+# results folder:
+result_folder = '/nas/home/fpicetti/dip_prnu_anonymizer/results/default'
+
+# output folder
+output_folder = '/nas/home/smandelli/Pycharm_projects/dip_prnu_anonymizer/results/default_results'
+
+psnr_min_vec = [37, 38, 39, 40]
+
+os.makedirs(output_folder, exist_ok=True)
+
+# load negative values.
+negative_ncc_path = '/nas/home/smandelli/ispg-greeneyes-data/icip2017_ImageAnonymizer/online_code/NCC_others.mat'
+ncc_from_sara = loadmat(negative_ncc_path)['ncc']
+# estimate the standard deviation of the negative distribution
+std_neg_ncc = np.std(ncc_from_sara.ravel())
+
+for dev_idx, dev in enumerate(devices):
+
+    # extract only npy files coming from this device.
+    list_npy_dev = sorted(glob(os.path.join(result_folder, '{}*_run.npy'.format(dev))))
+
+    anonymized_img_dev_params = np.zeros((len(psnr_min_vec), len(list_npy_dev), 2))
+
+    for r, npy_path in enumerate(list_npy_dev):
+
+        # load file
+        result = np.load(npy_path, allow_pickle=True).tolist()
+
+        # load psnr and ncc
+        psnr_list = result['history']['psnr']
+        idx_ascending_psnr = np.argsort(psnr_list)
+        psnr_array = np.asarray(psnr_list)
+        psnr_ascending = psnr_array[idx_ascending_psnr]   # psnr in ascending order.
+
+        ncc_list = result['history']['ncc_w']
+        ncc_ascending_psnr = np.asarray(ncc_list)[idx_ascending_psnr]  # ncc with the same order of psnr.
+
+        # loop over psnr.
+        for p, psnr_min in enumerate(psnr_min_vec):
+
+            psnr_idx_ok = np.where(psnr_ascending >= np.min([np.max(psnr_ascending), psnr_min]))[0]
+
+            ####################### limit the ncc to the +- 3sigma interval of negative values #########################
+            # ncc_idx_ok = np.where((ncc_ascending_psnr >= -3*std_neg_ncc) & (ncc_ascending_psnr <= 3*std_neg_ncc))[0]
+            #
+            # # intersection between the two sets.
+            # overall_idx_ok = np.where(np.in1d(ncc_idx_ok, psnr_idx_ok) == True)[0]
+            #
+            # if overall_idx_ok.size == 0:
+            #    overall_idx_ok = psnr_idx_ok
+
+            ############################################################################################################
+            overall_idx_ok = psnr_idx_ok
+
+            # select the index related to the minimum of ncc
+            ncc_psnr_ok_idx = ncc_ascending_psnr[overall_idx_ok].argmin()
+            ncc_psnr_ok = ncc_ascending_psnr[overall_idx_ok[ncc_psnr_ok_idx]]
+            psnr_ok = psnr_ascending[psnr_idx_ok[ncc_psnr_ok_idx]]
+
+            anonymized_img_dev_params[p, r, :] = [psnr_ok, ncc_psnr_ok]
+
+    # save results per device.
+    np.save(os.path.join(output_folder, 'anonymized_params_{}.npy'.format(dev)), anonymized_img_dev_params)
---
+++ results_script/show_results.py
@@ -0,0 +1,85 @@
+# show output results from DEEP PRNU ANONYMIZER:
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import loadmat
+from sklearn.metrics import roc_curve
+
+# devices:
+devices = ['Nikon_D200_0', 'Nikon_D200_1', 'Nikon_D70_0', 'Nikon_D70_1', 'Nikon_D70s_0', 'Nikon_D70s_1']
+
+# result folder
+output_folder = '/nas/home/smandelli/Pycharm_projects/dip_prnu_anonymizer/results/default_results'
+
+psnr_min_vec = [37, 38, 39, 40]
+
+negative_ncc_path = '/nas/home/smandelli/ispg-greeneyes-data/icip2017_ImageAnonymizer/online_code/NCC_others.mat'
+ncc_from_sara = loadmat(negative_ncc_path)['ncc']
+
+for p, psnr_min in enumerate(psnr_min_vec):
+
+    thr_values = []
+    psnr_vec = []
+
+    for dev_idx, dev in enumerate(devices):
+
+        # load DEEP PRIOR ANONYMIZATION results
+        anonymized_img_dev_params = np.load(os.path.join(output_folder, 'anonymized_params_{}.npy'.format(dev)))
+        # size = (len(psnr_min_vec), len(list_npy_dev), [psnr_value, ncc_value]))
+
+        psnr_vec += [anonymized_img_dev_params[p, :, 0]]
+
+        # load NEGATIVE NCC VALUES (from Sara, Matlab values): (500 values of ncc).
+        negative_ncc_values = ncc_from_sara[dev_idx, np.setdiff1d(np.arange(0, 6), dev_idx), :].ravel()
+
+        values_concat = np.concatenate((negative_ncc_values, anonymized_img_dev_params[p, :, 1].ravel()))
+
+        # update thr value
+        thr_values += [values_concat]
+
+    thr_values_arr = np.sort(np.unique(np.concatenate(thr_values).ravel()))
+
+    tn = np.zeros((len(devices), thr_values_arr.shape[0]))
+    fn = np.zeros((len(devices), thr_values_arr.shape[0]))
+    tp = np.zeros((len(devices), thr_values_arr.shape[0]))
+    fp = np.zeros((len(devices), thr_values_arr.shape[0]))
+
+    for dev_idx, dev in enumerate(devices):
+
+        anonymized_img_dev_params = np.load(os.path.join(output_folder, 'anonymized_params_{}.npy'.format(dev)))
+        tp_set = anonymized_img_dev_params[p, :, 1].ravel()
+        tn_set = ncc_from_sara[dev_idx, np.setdiff1d(np.arange(0, 6), dev_idx), :].ravel()
+
+        for t, thr in enumerate(thr_values_arr):
+
+            tn[dev_idx, t] = np.sum(tn_set <= thr)
+            fn[dev_idx, t] = np.sum(tp_set <= thr)
+            tp[dev_idx, t] = np.sum(tp_set > thr)
+            fp[dev_idx, t] = np.sum(tn_set > thr)
+
+    # ROC CURVE OF EAH DEVICE BY SKLEARN
+    #     fpr, tpr, _ = roc_curve(np.concatenate([np.ones_like(tp_set), np.zeros_like(tn_set)]),
+    #                             np.concatenate([tp_set, tn_set]))
+    #
+    #     plt.plot(fpr, tpr)
+    # plt.show()
+
+    tp_rate = np.mean(tp / (tp + fn), axis=0)
+    fp_rate = np.mean(fp / (fp + tn), axis=0)
+
+    # final ROC curve, averaged over devices
+
+    plt.plot(fp_rate, tp_rate)
+    plt.show()
+
+    tp_sort = np.sort(tp_rate)
+    fp_sort = np.sort(fp_rate)
+
+    tpr_001_ind = np.where(fp_sort >= 0.01)[0][0]
+    tpr_001 = tp_sort[tpr_001_ind]
+
+    print('min PSNR = {}, AUC = {}'.format(psnr_min, np.trapz(tp_sort, fp_sort)))
+    print('min PSNR = {}, TPR@0.01 = {}'.format(psnr_min, tpr_001))
+    print('min PSNR = {}, Median PSNR [dB] = {}'.format(psnr_min, np.median(np.concatenate(psnr_vec))))
+    print('min PSNR = {}, Mean PSNR [dB] = {}'.format(psnr_min, np.mean(np.concatenate(psnr_vec))))
